4f133a90008a6340996e193d9507d5bfd3e685f2
Aur Saraf <aur@sarafconsulting.com>
2022-08-30 19:13:11 +0300

    return string, pass test: wasm-pack test --headless --chrome

--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -9,11 +9,6 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rust-wasm!");
+pub fn greet() -> String {
+    "Hello, rust-wasm!".to_string()
 }
