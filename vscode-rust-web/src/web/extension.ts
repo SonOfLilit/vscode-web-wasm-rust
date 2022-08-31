25810d0c90a18c867b2081cf22365bf07ff93af4
Aur Saraf <aur@sarafconsulting.com>
2022-08-31 03:41:10 +0300

    whitespace

--- vscode-rust-web/src/web/extension.ts
+++ vscode-rust-web/src/web/extension.ts
@@ -1,26 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vscode-rust-web" is now active in the web extension host!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-rust-web" is now active in the web extension host!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "vscode-rust-web.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-rust-web.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from vscode-rust-web in a web extension host!"
+      );
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-rust-web in a web extension host!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
